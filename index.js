7cf262fa06c1facd49b8ae615b1062f0add140d0
Leo Trudel <leoltrudel@gmail.com>
2018-04-01 17:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var recipies = new Object({food: "bread"})
+var recipes = new Object ({})
 
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
